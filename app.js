b96f876361f0682df39e852ad90d5977bf34640e
Matt Morris <mcm0027@yahoo.com>
2016-02-01 10:03:18 -0600

    clean up question text for number 9

--- app.js
+++ app.js
@@ -299,7 +299,7 @@ var answersShowing = {
 // A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
 // a2 + b2 = c2
-// For example, 32 + 42 = 9 + 16 = 25 = 52.
+// For example, 3 squared + 4 squared = 9 + 16 = 25 = 5 squared.
 
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
@@ -316,6 +316,6 @@ var answersShowing = {
       }
     }
   }
-  console.log(cool)
+  console.log(cool);
 
   })();
